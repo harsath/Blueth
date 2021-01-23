b84eeb63e2e10a7e1fb2411c57b0f1996a5d32ba
harsathAI <harsath@tuta.io>
2021-01-23 00:36:23 -0500

    thread-safe queue impl added, yet to be tested

---
+++ blueth/concurrency/ThreadsafeQueue.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <queue>
+#include <mutex>
+#include <stdexcept>
+#include <optional>
+#include <type_traits>
+
+template<typename T> class thread_safe_queue {
+	private:
+	  std::queue<T> queue_;
+	  mutable std::mutex mutex_;
+	  [[nodiscard]] bool empty_() const noexcept {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      return queue_.empty();
+	  }
+	public:
+	  thread_safe_queue() = default;
+	  thread_safe_queue(const thread_safe_queue<T>&) = delete;
+	  thread_safe_queue& operator=(const thread_safe_queue<T>&) = delete;
+	  thread_safe_queue(thread_safe_queue<T>&& queue_move) noexcept(false) {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      if(!empty_()){ throw std::logic_error("Moving into a non-empty queue"); }
+	      queue_(std::move(queue_move));
+	  }
+	  [[nodiscard]] std::size_t size() const noexcept {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      return queue_.size();
+	  }
+	  [[nodiscard]] std::optional<T> pop() noexcept {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      if(queue_.empty()){ return std::nullopt; }
+	      T returner{std::move(queue_.front())};
+	      queue_.pop();
+	      return returner;
+	  }
+	  void push(const T& data) noexcept {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      queue_.push(data);
+	  }
+	  void push(T&& data) noexcept {
+	      std::lock_guard<std::mutex> op_lock{mutex_};
+	      queue_.emplace(std::move(data));
+	  }
+};
