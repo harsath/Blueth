74ddc080c01edde70c025cc5d62702182e9ecc0b
harsath <harsath@tuta.io>
2021-03-20 17:01:07 -0400

    simd char_vector_16_sse2 finishup with 'friend' functions

--- blueth/simd.hpp
+++ blueth/simd.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <emmintrin.h>
-#include <stdint.h>
+#include <iostream>
 
 #if defined(__clang__) || defined(__GNUC__)
 #define BLUETH_FORCE_INLINE [[gnu::always_inline]]
@@ -29,6 +29,27 @@ namespace blueth::simd {
 		  BLUETH_FORCE_INLINE uint32_t mask() const noexcept {
 			  return static_cast<uint32_t>(_mm_movemask_epi8(data_));
 		  }
+	};
+	class char_vector_16_sse2{
+		private:
+		  __m128i data_;
+		public:
+		  static constexpr int size = 16;
+		  BLUETH_FORCE_INLINE explicit char_vector_16_sse2(__m128i data) noexcept
+			  : data_{data} {}
+		  BLUETH_FORCE_INLINE friend bool_vector_16_sse2 operator==(char_vector_16_sse2 x, char_vector_16_sse2 y) noexcept {
+			  return bool_vector_16_sse2(_mm_cmpeq_epi8(x.data_, y.data_));
+		  }
+		  BLUETH_FORCE_INLINE friend bool_vector_16_sse2 operator<(char_vector_16_sse2 x, char_vector_16_sse2 y) noexcept {
+			  return bool_vector_16_sse2(_mm_cmplt_epi8(x.data_, y.data_));
+		  }
+		  BLUETH_FORCE_INLINE friend bool_vector_16_sse2 operator>(char_vector_16_sse2 x, char_vector_16_sse2 y) noexcept {
+			  return bool_vector_16_sse2(_mm_cmpgt_epi8(x.data_, y.data_));
+		  }
+		  BLUETH_FORCE_INLINE friend bool_vector_16_sse2 operator|(char_vector_16_sse2 x, char_vector_16_sse2 y) noexcept {
+			  return bool_vector_16_sse2(_mm_or_si128(x.data_, y.data_));
+		  }
+		  BLUETH_FORCE_INLINE __m128i m128i() const noexcept { return data_; }
 	};
 
 } // end namespace blueth::simd
