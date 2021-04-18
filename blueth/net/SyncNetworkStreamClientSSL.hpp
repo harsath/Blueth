ccfebb1ddad19e14df393b527cfb7f3cff183036
harsath <harsath@tuta.io>
2021-04-18 02:21:36 -0400

    SyncNetworkStreamClientSSL support and deleters for wolfSSL objs
    Need to write tests for the draft SyncNetworkStreamClientSSL
    implementation. Probably a Perl script?

--- blueth/net/SyncNetworkStreamClientSSL.hpp
+++ blueth/net/SyncNetworkStreamClientSSL.hpp
@@ -2,23 +2,24 @@
 #include "NetworkStream.hpp"
 #include "common.hpp"
 #include "io/IOBuffer.hpp"
+#include "utils/SSLHelpers.hpp"
 #include "utils/UnixTime.hpp"
 #include <cstdint>
-#include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <functional>
-#include <memory>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdexcept>
-#include <strings.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <utility>
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
 
 namespace blueth::net {
 
-class SyncNetworkStreamClient final : public NetworkStream<char> {
+class SyncNetworkStreamClientSSL final : public NetworkStream<char> {
+      private:
 	std::string endpoint_host_;
 	std::uint16_t endpoint_port_;
 	int endpoint_fd_;
@@ -32,24 +33,26 @@ class SyncNetworkStreamClient final : public NetworkStream<char> {
 	    nullptr};
 	std::unique_ptr<io::IOBuffer<char>> io_buffer_{nullptr};
 	bool is_connected_{false};
+	std::unique_ptr<WOLFSSL_CTX, detail::WolfSSL_CTX_Deleter> ctx_{nullptr};
+	std::unique_ptr<WOLFSSL, detail::WolfSSL_Deleter> ssl_{nullptr};
 
       public:
 	constexpr static std::size_t default_io_buffer_size = 2048;
 	static std::unique_ptr<NetworkStream<char>>
 	create(std::string &&endpoint_host, std::uint16_t &&endpoint_port,
 	       StreamProtocol &&stream_protocol) {
-		return std::make_unique<SyncNetworkStreamClient>(
+		return std::make_unique<SyncNetworkStreamClientSSL>(
 		    std::forward<std::string>(endpoint_host),
 		    std::forward<std::uint16_t>(endpoint_port),
 		    std::forward<StreamProtocol>(stream_protocol));
 	}
-	SyncNetworkStreamClient(std::string endpoint_host,
-				std::uint16_t endpoint_port,
-				StreamProtocol stream_protocol) noexcept(false);
+	SyncNetworkStreamClientSSL(
+	    std::string endpoint_host, std::uint16_t endpoint_port,
+	    StreamProtocol stream_protocol) noexcept(false);
 	int streamRead(size_t read_length) noexcept(false) override;
 	int streamWrite(const std::string &data) noexcept(false) override;
 	void flushBuffer() noexcept(false) override;
-	BLUETH_NODISCARD buffer_type getIOBuffer() noexcept override;
+	BLUETH_NODISCARD buffer_type getIOBuffer() noexcept(false) override;
 	BLUETH_NODISCARD const_buffer_reference_type
 	constGetIOBuffer() const noexcept override;
 	void setIOBuffer(buffer_type io_buffer) noexcept override;
@@ -65,26 +68,21 @@ class SyncNetworkStreamClient final : public NetworkStream<char> {
 	std::function<void(const_buffer_reference_type)>
 	getWriteCallback() const noexcept override;
 	void closeConnection() noexcept(false) override;
-	~SyncNetworkStreamClient();
+	~SyncNetworkStreamClientSSL();
 };
 
-SyncNetworkStreamClient::SyncNetworkStreamClient(
+// @@@ Right now, the Sync SSL-Stream is TCP only
+SyncNetworkStreamClientSSL::SyncNetworkStreamClientSSL(
     std::string endpoint_host, std::uint16_t endpoint_port,
     StreamProtocol stream_protocol) noexcept(false)
     : endpoint_host_{std::move(endpoint_host)}, endpoint_port_{endpoint_port},
       stream_protocol_{stream_protocol}, stream_mode_{StreamMode::Client},
-      stream_type_{StreamType::SyncStream} {
+      stream_type_{StreamType::SSLSyncStream} {
 	::hostent *hoste;
 	::sockaddr_in addr;
-	if ((hoste = ::gethostbyname(endpoint_host_.c_str())) == nullptr) {
+	endpoint_fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+	if ((hoste = ::gethostbyname(endpoint_host.c_str())) == nullptr) {
 		throw std::runtime_error{std::strerror(errno)};
-	}
-	if (stream_protocol_ == StreamProtocol::TCP) {
-		endpoint_fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
-	} else if (stream_protocol_ == StreamProtocol::UDP) {
-		endpoint_fd_ = ::socket(AF_INET, SOCK_DGRAM, 0);
-	} else {
-		throw std::runtime_error{"invalid StreamProtocol"};
 	}
 	if (endpoint_fd_ < 0) {
 		std::perror("socket()");
@@ -101,20 +99,52 @@ SyncNetworkStreamClient::SyncNetworkStreamClient(
 		std::perror("connect()");
 		throw std::runtime_error{std::strerror(errno)};
 	}
+	int ret = wolfSSL_Init();
+	if (ret != WOLFSSL_SUCCESS) {
+		fprintf(stderr, "Error: Failed to init wolfSSL lib\n");
+		close(endpoint_fd_);
+		throw std::runtime_error{"wolfSSL_Init()"};
+	}
+	ctx_ = std::unique_ptr<WOLFSSL_CTX, detail::WolfSSL_CTX_Deleter>(
+	    wolfSSL_CTX_new(wolfTLSv1_2_client_method()));
+	if (ctx_.get() == nullptr) {
+		fprintf(stderr, "Error: failed to create WOLFSSL_CTX object\n");
+		close(endpoint_fd_);
+		throw std::runtime_error{"wolfSSL_CTX_new()"};
+	}
+	ssl_ = std::unique_ptr<WOLFSSL, detail::WolfSSL_Deleter>(
+	    wolfSSL_new(ctx_.get()));
+	if (ssl_.get() == nullptr) {
+		fprintf(stderr, "Error: failed to create new WOLFSSL object\n");
+		close(endpoint_fd_);
+		throw std::runtime_error{"wolfSSL_new()"};
+	}
+	ret = wolfSSL_set_fd(ssl_.get(), endpoint_fd_);
+	if (ret != SSL_SUCCESS) {
+		fprintf(stderr, "Error: failed to set FD to SSL context\n");
+		close(endpoint_fd_);
+		throw std::runtime_error{"wolfSSL_set_fd()"};
+	}
+	ret = wolfSSL_connect(ssl_.get());
+	if (ret != SSL_SUCCESS) {
+		fprintf(stderr, "Error: failed to connect to server\n");
+		close(endpoint_fd_);
+		throw std::runtime_error{"wolfSSL_connect()"};
+	}
 	io_buffer_ = io::IOBuffer<char>::create(default_io_buffer_size);
 	connected_time_ = UnixTime();
 	is_connected_ = true;
 }
 
-int SyncNetworkStreamClient::streamRead(size_t read_length) noexcept(false) {
+int SyncNetworkStreamClientSSL::streamRead(size_t read_length) noexcept(false) {
 	if (!io_buffer_ || !is_connected_) {
 		throw std::runtime_error{"invalid IOBuffer or the connection "
-					 "to the endpoint is closed"};
+					 "to the SSL endpoint is closed"};
 	}
 	if (read_length > io_buffer_->getAvailableSpace())
 		io_buffer_->reserve(io_buffer_->getCapacity() + read_length);
 	char temp_buffer[read_length];
-	int read_ret = ::recv(endpoint_fd_, temp_buffer, read_length, 0);
+	int read_ret = wolfSSL_read(ssl_.get(), temp_buffer, read_length);
 	if (read_ret < 0) return read_ret;
 	io_buffer_->appendRawBytes(temp_buffer, read_ret);
 	if (read_callback_) read_callback_(io_buffer_);
@@ -122,82 +152,79 @@ int SyncNetworkStreamClient::streamRead(size_t read_length) noexcept(false) {
 }
 
 /**
- * Since it's a blocking-IO byte transfer on the wire, we don't need to touch
- * the underlying IOBuffer so we only need to check if we transfered the bytes
- * directly from 'data' onto the wire. Using a IOBuffer is a level of
- * indirection, which we don't want since this interface is Sync and not Async
- * network-IO.
+ * Since this is a Sync-transfer on the wire, the call gets blocked until all
+ * the bytes are sent on the wire. Since it's a blocking-IO, we don't need to
+ * touch the IOBuffer. So we only need to check if we actually have connection
+ * open the the SSL endpoint.
  */
-
-int SyncNetworkStreamClient::streamWrite(const std::string &data) noexcept(
+int SyncNetworkStreamClientSSL::streamWrite(const std::string &data) noexcept(
     false) {
 	if (!is_connected_) {
 		throw std::runtime_error{
-		    "The TCP connection to the endpoint is not connected"};
+		    "The connection to the SSL endpoint is closed"};
 	}
 	if (data.empty()) return 0;
-	int write_ret = ::send(endpoint_fd_, data.c_str(), data.size(), 0);
+	int write_ret = ::wolfSSL_write(ssl_.get(), data.c_str(), data.size());
 	if (write_callback_) write_callback_(io_buffer_);
 	return write_ret;
 }
 
-void SyncNetworkStreamClient::flushBuffer() noexcept(false) {
+void SyncNetworkStreamClientSSL::flushBuffer() noexcept(false) {
 	if (!io_buffer_) { throw std::runtime_error{"invalid IOBuffer"}; }
 	io_buffer_->clear();
 }
 
-BLUETH_NODISCARD SyncNetworkStreamClient::buffer_type
-SyncNetworkStreamClient::getIOBuffer() noexcept {
+BLUETH_NODISCARD SyncNetworkStreamClientSSL::buffer_type
+SyncNetworkStreamClientSSL::getIOBuffer() noexcept(false) {
 	std::unique_ptr<io::IOBuffer<char>> temp_buffer_holder =
 	    std::move(io_buffer_);
 	io_buffer_ = nullptr;
 	return std::move(temp_buffer_holder);
 }
 
-BLUETH_NODISCARD SyncNetworkStreamClient::const_buffer_reference_type
-SyncNetworkStreamClient::constGetIOBuffer() const noexcept {
+BLUETH_NODISCARD SyncNetworkStreamClientSSL::const_buffer_reference_type
+SyncNetworkStreamClientSSL::constGetIOBuffer() const noexcept {
 	return io_buffer_;
 }
 
-void SyncNetworkStreamClient::setIOBuffer(
-    SyncNetworkStreamClient::buffer_type io_buffer) noexcept {
+void SyncNetworkStreamClientSSL::setIOBuffer(
+    SyncNetworkStreamClientSSL::buffer_type io_buffer) noexcept {
 	io_buffer_ = std::move(io_buffer);
 }
 
-StreamType SyncNetworkStreamClient::getStreamType() const noexcept {
+StreamType SyncNetworkStreamClientSSL::getStreamType() const noexcept {
 	return stream_type_;
 }
 
-StreamMode SyncNetworkStreamClient::getStreamMode() const noexcept {
+StreamMode SyncNetworkStreamClientSSL::getStreamMode() const noexcept {
 	return stream_mode_;
 }
 
-StreamProtocol SyncNetworkStreamClient::getStreamProtocol() const noexcept {
+StreamProtocol SyncNetworkStreamClientSSL::getStreamProtocol() const noexcept {
 	return stream_protocol_;
 }
 
-void SyncNetworkStreamClient::setReadCallback(
-    std::function<void(SyncNetworkStreamClient::const_buffer_reference_type)>
-	callback_fn) noexcept {
+void SyncNetworkStreamClientSSL::setReadCallback(
+    std::function<void(const_buffer_reference_type)> callback_fn) noexcept {
 	read_callback_ = std::move(callback_fn);
 }
 
-void SyncNetworkStreamClient::setWriteCallback(
+void SyncNetworkStreamClientSSL::setWriteCallback(
     std::function<void(const_buffer_reference_type)> callback_fn) noexcept {
 	write_callback_ = std::move(callback_fn);
 }
 
-std::function<void(SyncNetworkStreamClient::const_buffer_reference_type)>
-SyncNetworkStreamClient::getReadCallback() const noexcept {
+std::function<void(SyncNetworkStreamClientSSL::const_buffer_reference_type)>
+SyncNetworkStreamClientSSL::getReadCallback() const noexcept {
 	return read_callback_;
 }
 
-std::function<void(SyncNetworkStreamClient::const_buffer_reference_type)>
-SyncNetworkStreamClient::getWriteCallback() const noexcept {
+std::function<void(SyncNetworkStreamClientSSL::const_buffer_reference_type)>
+SyncNetworkStreamClientSSL::getWriteCallback() const noexcept {
 	return write_callback_;
 }
 
-void SyncNetworkStreamClient::closeConnection() noexcept(false) {
+void SyncNetworkStreamClientSSL::closeConnection() noexcept(false) {
 	if (is_connected_) {
 		int ret = ::close(endpoint_fd_);
 		if (ret < 0) { throw std::runtime_error{std::strerror(errno)}; }
@@ -205,6 +232,8 @@ void SyncNetworkStreamClient::closeConnection() noexcept(false) {
 	}
 }
 
-SyncNetworkStreamClient::~SyncNetworkStreamClient() { this->closeConnection(); }
+SyncNetworkStreamClientSSL::~SyncNetworkStreamClientSSL() {
+	this->closeConnection();
+}
 
 } // namespace blueth::net
